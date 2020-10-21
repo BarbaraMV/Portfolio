eb4c8b5bba2244a914d1d2adc59fde1e9e014bac
BarbaraMV <bmadariagav@gmail.com>
2020-10-21 07:57:58 -0300

    Arreglo de links en proyectos y modificacion de CV

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+
+console.log('antes de todo')
 const modals = document.getElementsByClassName("modal");
+console.log("antes del for")
 
 for(let i = 0; i < modals.length; i++) {
     let modal = modals[i];
@@ -7,11 +10,14 @@ for(let i = 0; i < modals.length; i++) {
     let modalImg = document.getElementById("img" + i);
     let captionText = document.getElementsByClassName("caption")[i];
     
+    console.log('antes del img onclick')
     img.onclick = function () {
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        modalImg.alt = this.alt;
+      modal.style.display = "block";
+      /*  modalImg.src = this.src;
+        console.log('ACA hHAY ALGO MALO')*/
+       /* modalImg.alt = this.alt;*/
         captionText.innerHTML = this.alt;
+        console.log("ACA HAY OTRA COSA MAS MALA XD")
     }
     
     let span = document.getElementsByClassName("close")[i];
@@ -20,3 +26,4 @@ for(let i = 0; i < modals.length; i++) {
         modal.style.display = "none";
     }
 }
+console.log("despues del for")
